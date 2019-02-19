e3ac3625b5bd0ad4a11d0408a0890800f7bb328f
Henrik Sandberg <henrik.a.sandberg@icloud.com>
2019-02-19 19:00:51 +0100

    Initial Commit

---
+++ BrickBraker/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BrickBraker
+//
+//  Created by Henrik Anthony Odden Sandberg on 2/19/19.
+//  Copyright © 2019 Henrik Anthony Odden Sandberg. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
