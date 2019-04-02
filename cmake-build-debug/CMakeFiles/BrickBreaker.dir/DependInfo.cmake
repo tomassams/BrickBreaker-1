7e22cbc276f636855850405e66b703b99e984bed
Henrik Sandberg <henrik.a.sandberg@icloud.com>
2019-04-02 14:21:20 +0200

    Update gitIgnore

--- cmake-build-debug/CMakeFiles/BrickBreaker.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/BrickBreaker.dir/DependInfo.cmake
@@ -4,18 +4,18 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "C:/school/bb2/BrickBreaker/src/cppfiles/Ball.cpp" "C:/school/bb2/BrickBreaker/cmake-build-debug/CMakeFiles/BrickBreaker.dir/src/cppfiles/Ball.cpp.obj"
-  "C:/school/bb2/BrickBreaker/src/cppfiles/Bricks.cpp" "C:/school/bb2/BrickBreaker/cmake-build-debug/CMakeFiles/BrickBreaker.dir/src/cppfiles/Bricks.cpp.obj"
-  "C:/school/bb2/BrickBreaker/src/cppfiles/GameManager.cpp" "C:/school/bb2/BrickBreaker/cmake-build-debug/CMakeFiles/BrickBreaker.dir/src/cppfiles/GameManager.cpp.obj"
-  "C:/school/bb2/BrickBreaker/src/cppfiles/InputManager.cpp" "C:/school/bb2/BrickBreaker/cmake-build-debug/CMakeFiles/BrickBreaker.dir/src/cppfiles/InputManager.cpp.obj"
-  "C:/school/bb2/BrickBreaker/src/cppfiles/Paddle.cpp" "C:/school/bb2/BrickBreaker/cmake-build-debug/CMakeFiles/BrickBreaker.dir/src/cppfiles/Paddle.cpp.obj"
-  "C:/school/bb2/BrickBreaker/src/main.cpp" "C:/school/bb2/BrickBreaker/cmake-build-debug/CMakeFiles/BrickBreaker.dir/src/main.cpp.obj"
+  "/Users/henrik/Documents/GitHub/BrickBreaker/src/cppfiles/Ball.cpp" "/Users/henrik/Documents/GitHub/BrickBreaker/cmake-build-debug/CMakeFiles/BrickBreaker.dir/src/cppfiles/Ball.cpp.o"
+  "/Users/henrik/Documents/GitHub/BrickBreaker/src/cppfiles/Bricks.cpp" "/Users/henrik/Documents/GitHub/BrickBreaker/cmake-build-debug/CMakeFiles/BrickBreaker.dir/src/cppfiles/Bricks.cpp.o"
+  "/Users/henrik/Documents/GitHub/BrickBreaker/src/cppfiles/GameManager.cpp" "/Users/henrik/Documents/GitHub/BrickBreaker/cmake-build-debug/CMakeFiles/BrickBreaker.dir/src/cppfiles/GameManager.cpp.o"
+  "/Users/henrik/Documents/GitHub/BrickBreaker/src/cppfiles/InputManager.cpp" "/Users/henrik/Documents/GitHub/BrickBreaker/cmake-build-debug/CMakeFiles/BrickBreaker.dir/src/cppfiles/InputManager.cpp.o"
+  "/Users/henrik/Documents/GitHub/BrickBreaker/src/cppfiles/Paddle.cpp" "/Users/henrik/Documents/GitHub/BrickBreaker/cmake-build-debug/CMakeFiles/BrickBreaker.dir/src/cppfiles/Paddle.cpp.o"
+  "/Users/henrik/Documents/GitHub/BrickBreaker/src/main.cpp" "/Users/henrik/Documents/GitHub/BrickBreaker/cmake-build-debug/CMakeFiles/BrickBreaker.dir/src/main.cpp.o"
   )
-set(CMAKE_CXX_COMPILER_ID "GNU")
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  "C:/TDM-GCC-64/include/SDL2"
+  "/usr/local/include/SDL2"
   )
 
 # Targets to which this target links.
--- cmake-build-debug/CMakeFiles/BrickBreaker.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/BrickBreaker.dir/cmake_clean.cmake
@@ -1,14 +1,12 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/BrickBreaker.dir/src/main.cpp.obj"
-  "CMakeFiles/BrickBreaker.dir/src/cppfiles/Ball.cpp.obj"
-  "CMakeFiles/BrickBreaker.dir/src/cppfiles/GameManager.cpp.obj"
-  "CMakeFiles/BrickBreaker.dir/src/cppfiles/Bricks.cpp.obj"
-  "CMakeFiles/BrickBreaker.dir/src/cppfiles/InputManager.cpp.obj"
-  "CMakeFiles/BrickBreaker.dir/src/cppfiles/Paddle.cpp.obj"
+  "CMakeFiles/BrickBreaker.dir/src/main.cpp.o"
+  "CMakeFiles/BrickBreaker.dir/src/cppfiles/Ball.cpp.o"
+  "CMakeFiles/BrickBreaker.dir/src/cppfiles/GameManager.cpp.o"
+  "CMakeFiles/BrickBreaker.dir/src/cppfiles/Bricks.cpp.o"
+  "CMakeFiles/BrickBreaker.dir/src/cppfiles/InputManager.cpp.o"
+  "CMakeFiles/BrickBreaker.dir/src/cppfiles/Paddle.cpp.o"
   "BrickBreaker.pdb"
-  "BrickBreaker.exe"
-  "BrickBreaker.exe.manifest"
-  "libBrickBreaker.dll.a"
+  "BrickBreaker"
 )
 
 # Per-language clean rules from dependency scanning.
